8c1e780ad6d31d1b26fd139b1af19e0b530bccb9
Anurag Kejriwal <anuragkejriwal1@gmail.com>
2022-05-18 10:19:30 -0400

    Initial Commit

---
+++ SwifUIWarCardGame/SwifUIWarCardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwifUIWarCardGameApp.swift
+//  SwifUIWarCardGame
+//
+//  Created by Anurag Kejriwal on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SwifUIWarCardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
